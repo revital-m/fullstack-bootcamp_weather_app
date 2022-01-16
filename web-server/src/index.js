ce4adbbe4d2f834e96b166493b345b4759176398
revital-m <revital13m@gmail.com>
2022-01-16 15:22:29 +0200

    deploying to heroku

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -5,20 +5,9 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 require('dotenv').config();
 
+const MY_PORT = process.env.PORT || 5000;
+
 const app = express();
-
-// Define paths for Express config
-// const publicDirectoryPath = path.join(__dirname, '../public')
-// const viewsPath = path.join(__dirname, '../templates/views')
-// const partialsPath = path.join(__dirname, '../templates/partials')
-
-// Setup handlebars engine and views location
-// app.set('view engine', 'hbs')
-// app.set('views', viewsPath)
-// hbs.registerPartials(partialsPath)
-
-// Setup static directory to serve
-// app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
@@ -54,7 +43,7 @@ app.get('*', (req, res) => {
     })
 })
 
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server is up on port ${PORT}.`)
+
+app.listen(MY_PORT, () => {
+    console.log(`Server is up on port ${MY_PORT}.`)
 })
